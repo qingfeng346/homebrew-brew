f1d5e39293e43ed89834877ddc8a5d52fb15d80d
linyuan.yang <linyuan.yang@centurygame.com>
2022-03-19 14:26:23 +0800

    update

--- Formula/stools.rb
+++ Formula/stools.rb
@@ -3,20 +3,20 @@ class Sco < Formula
     homepage "https://github.com/qingfeng346/stools"
     license "GPL-3.0"
     if OS.mac? && Hardware::CPU.intel?
-      url "https://github.com/qingfeng346/Scorpio-CSharp/releases/download/v2.3.7/sco-2.3.7-osx-x64.zip"
-      sha256 "B6F24287EDCCE2872BFE194A75BD286D3D9DD34C5EEBFABB3EE1EA2BF33ECFF5"
+      url "https://github.com/qingfeng346/stools/releases/download/v1.0.0/stools-1.0.0-osx-x64.zip"
+      sha256 "E76ACADFAEDBE83E7E3424BCF7CAFA4EAC758862F069D121E40D882CE959E350"
     end
     if OS.mac? && Hardware::CPU.arm?
-      url "https://github.com/qingfeng346/Scorpio-CSharp/releases/download/v2.3.7/sco-2.3.7-osx-arm64.zip"
-      sha256 "9361CB4845588DFD7E2C217DD13C13C518F27A9706C7E98269ACEA3EB64AD902"
+      url "https://github.com/qingfeng346/stools/releases/download/v1.0.0/stools-1.0.0-osx-arm64.zip"
+      sha256 "DA879403FEBC91F916BD5702F1BA6FB5274BBCBC0ACD8375908063CC4748BA66"
     end
     if OS.linux? && Hardware::CPU.intel?
-      url "https://github.com/qingfeng346/Scorpio-CSharp/releases/download/v2.3.7/sco-2.3.7-linux-x64.zip"
-      sha256 "621A6C77C24B4B1C348B5A36B1ABDCD2D0F2CEBCA75EA8E340466B1ADE5AE019"
+      url "https://github.com/qingfeng346/stools/releases/download/v1.0.0/stools-1.0.0-linux-x64.zip"
+      sha256 "D8A21B42CA16F6EED42C5755A35533D666D3742D082B3CB9614A2DA684215BDF"
     end
     if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/qingfeng346/Scorpio-CSharp/releases/download/v2.3.7/sco-2.3.7-linux-arm64.zip"
-      sha256 "887770420FC866D594919B6F402D2C7FB69558893D68D2C8FF166F5ECF2C5B41"
+      url "https://github.com/qingfeng346/stools/releases/download/v1.0.0/stools-1.0.0-linux-arm64.zip"
+      sha256 "70D1A495086594B0608BF2F95D71B92159D72FC4B081C8E2880A5F18BCA04946"
     end
     def install
       prefix.install Dir["*"]
