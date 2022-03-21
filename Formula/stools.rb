3505cd554cc707e74798c39ad02dec67e0ec3a46
linyuan.yang <linyuan.yang@centurygame.com>
2022-03-21 18:43:41 +0800

    stools

--- Formula/stools.rb
+++ Formula/stools.rb
@@ -4,19 +4,19 @@ class Stools < Formula
     license "GPL-3.0"
     if OS.mac? && Hardware::CPU.intel?
       url "https://github.com/qingfeng346/stools/releases/download/v1.0.0/stools-1.0.0-osx-x64.zip"
-      sha256 "E76ACADFAEDBE83E7E3424BCF7CAFA4EAC758862F069D121E40D882CE959E350"
+      sha256 "0B1BDFF5C3C2472A0F4332E57E76ABF74D582F341BDDCD13CFEF5492BE661803"
     end
     if OS.mac? && Hardware::CPU.arm?
       url "https://github.com/qingfeng346/stools/releases/download/v1.0.0/stools-1.0.0-osx-arm64.zip"
-      sha256 "DA879403FEBC91F916BD5702F1BA6FB5274BBCBC0ACD8375908063CC4748BA66"
+      sha256 "3AB569C56BDC75DDA30A7C31FD2E129CF0AF7029FA2141F7BEAC5198D3A2CFAE"
     end
     if OS.linux? && Hardware::CPU.intel?
       url "https://github.com/qingfeng346/stools/releases/download/v1.0.0/stools-1.0.0-linux-x64.zip"
-      sha256 "D8A21B42CA16F6EED42C5755A35533D666D3742D082B3CB9614A2DA684215BDF"
+      sha256 "EDD50C6CD10DE344E9E5C657CF95C1A6FA0B21D7621DC37E97D30C9E97882569"
     end
     if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/qingfeng346/stools/releases/download/v1.0.0/stools-1.0.0-linux-arm64.zip"
-      sha256 "70D1A495086594B0608BF2F95D71B92159D72FC4B081C8E2880A5F18BCA04946"
+      sha256 "031683227B36061C800D729288B78A23F236B218039F0627DA3389F83D8962AA"
     end
     def install
       prefix.install Dir["*"]
