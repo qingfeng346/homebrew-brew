d37c1dcb19f7cdc8a6d815edbb85af5de2b2528f
while <qingfeng346@126.com>
2022-08-14 21:24:25 +0800

    stools 1.0.2

--- Formula/stools.rb
+++ Formula/stools.rb
@@ -3,20 +3,20 @@ class Stools < Formula
     homepage "https://github.com/qingfeng346/stools"
     license "GPL-3.0"
     if OS.mac? && Hardware::CPU.intel?
-      url "https://github.com/qingfeng346/stools/releases/download/v1.0.1/stools-1.0.1-osx-x64.zip"
-      sha256 "F499972D32D3120A9C48971513E8AB359801A34EFF94CEF1EF6ED8184F3C5E41"
+      url "https://github.com/qingfeng346/stools/releases/download/v1.0.2/stools-1.0.2-osx-x64.zip"
+      sha256 "0A81275A06AF075D8351B79159183CB52A0B29F46BD9E6138DFD94B45720058A"
     end
     if OS.mac? && Hardware::CPU.arm?
-      url "https://github.com/qingfeng346/stools/releases/download/v1.0.1/stools-1.0.1-osx-arm64.zip"
-      sha256 "5214505C430D29ADFA87D68E4930C0813F099521FFB8E0D4500B0D6A2B65DEFD"
+      url "https://github.com/qingfeng346/stools/releases/download/v1.0.2/stools-1.0.2-osx-arm64.zip"
+      sha256 "4BD7529B39B7BA76FEB293969138DB75DB8774FE1A9681AA2A3D80FE1A709B19"
     end
     if OS.linux? && Hardware::CPU.intel?
-      url "https://github.com/qingfeng346/stools/releases/download/v1.0.1/stools-1.0.1-linux-x64.zip"
-      sha256 "BA8A769C4BF0330A1B7BC311F35EEA3853F8A85B2F70D61D4BD28029998AFD73"
+      url "https://github.com/qingfeng346/stools/releases/download/v1.0.2/stools-1.0.2-linux-x64.zip"
+      sha256 "A330C1D40F09AAABE47D9078B4593A69DD647D2A8234E7EA5E84CB255D50F4B8"
     end
     if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/qingfeng346/stools/releases/download/v1.0.1/stools-1.0.1-linux-arm64.zip"
-      sha256 "79C8DB85C74BA01777B74EBE0742F0A9732109E1618F51ECD2DFC35A735BD4E9"
+      url "https://github.com/qingfeng346/stools/releases/download/v1.0.2/stools-1.0.2-linux-arm64.zip"
+      sha256 "5DAC0F13BFB1AA060982C7933AE2BAFF20D502C3709CF6C76DC0AC1DD3221D18"
     end
     def install
       prefix.install Dir["*"]
